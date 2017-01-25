6ac44c0e73a9597ba069dd6e479b526392f4e060
VytautasSipavicius <Vytautassi321@nemi.tskoli.is>
2017-01-25 13:53:17 +0000

    Dæmi tvö klárað

--- Git_Verkefni_Skil.py
+++ Git_Verkefni_Skil.py
@@ -9,3 +9,10 @@ tala1_2 = int(input("Sláðu inn aðra tölu "))
 #Reikna og prenta út margfeldi og summu talnanna
 print("Summa talnnan er:", tala1_1 + tala1_2)
 print("Margfeldi talnnan er:", tala1_1 * tala1_2)
+
+#Dæmi 2
+#Búa til tvær breytur sem taka inn fornafn og eftirnafn
+fornafn = input("Sláðu inn Fornafnið þitt ")
+efirnafn = input("Sláðu inn Eftirnafnið þitt ")
+#prenta út nafnið með halló
+print("Halló",fornafn, efirnafn)
