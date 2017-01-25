33d2b3dd3b7dd5c8a68f65a8d45f834619fc308b
VytautasSipavicius <Vytautassi321@nemi.tskoli.is>
2017-01-25 13:51:02 +0000

    Dæmi eitt klárað

--- Git_Verkefni_Skil.py
+++ Git_Verkefni_Skil.py
@@ -3,5 +3,9 @@
 #Lausn við Git verkefni
 
 #Dæmi 1
+#Búa til tvær breytur sem taka inn tölur frá notenda
 tala1_1 = int(input("Sláðu inn tölu "))
 tala1_2 = int(input("Sláðu inn aðra tölu "))
+#Reikna og prenta út margfeldi og summu talnanna
+print("Summa talnnan er:", tala1_1 + tala1_2)
+print("Margfeldi talnnan er:", tala1_1 * tala1_2)
